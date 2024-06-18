8d283662352020e9fd24f573afcedc9ddd207d0a
Kamil Zych <64442316+qerkses@users.noreply.github.com>
2024-06-18 11:54:04 +0200

    Add files via upload

---
+++ BookshelfWeb/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using BookshelfWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookshelfWeb.Controllers
+{
+    public class BooksController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7238/api");
+        private readonly HttpClient _httpClient;
+
+        public BooksController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<BookViewModel> booksList = new List<BookViewModel>();
+
+            try
+            {
+                var response = await _httpClient.GetAsync(_httpClient.BaseAddress + "/Books");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                booksList = JsonConvert.DeserializeObject<List<BookViewModel>>(content);
+            }
+            catch (Exception)
+            {
+           
+            }
+
+            return View(booksList);
+        }
+    }
+}
